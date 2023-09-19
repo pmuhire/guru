d68687c91af4ae7a023807573bd35e3253d7130d
Mwitero <pmuhire2002@gmail.com>
2023-09-19 02:46:24 +0200

    added the featured and category UI implementation

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import styles from "./categoryList.module.css";
+import Link from 'next/link';
+import Image from "next/image";
+
 
 export default function CategoryList() {
-  return (
-    <div>
-      CategoryList
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Categories</h1>
+            <div className={styles.categories}>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Style
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Fashion
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Food
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Travel
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Culture
+                </Link>
+                <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                    <Image src="/style.png" alt="" width={32} height={32} className={styles.image} />
+                    Coding
+                </Link>
+            </div>
+        </div>
+    )
 }
